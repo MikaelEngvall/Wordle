db13c5031eea632e6df25a4d878f5715c2200291
MikaelEngvall <mikael.engvall.me@gmail.com>
2024-11-16 19:48:26 +0100

    minor

--- translation.js
+++
@@ -1,7 +0,0 @@
-let translations = {
-    en: { title: 'Wordle', modeToggler: 'Switch to Dark Mode', playAgain: 'Play Again' },
-    it: { title: 'Parola', modeToggler: 'Passa alla Modalità Scura', playAgain: 'Gioca Ancora' },
-    fr: { title: 'Motus', modeToggler: 'Passer en Mode Sombre', playAgain: 'Rejouer' },
-    es: { title: 'Palabra', modeToggler: 'Cambiar a Modo Oscuro', playAgain: 'Jugar Otra Vez' },
-    de: { title: 'Wortspiel', modeToggler: 'Zum Dunkelmodus Wechseln', playAgain: 'Nochmal Spielen' },
-};
